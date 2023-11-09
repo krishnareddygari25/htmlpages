ff40d5df355f58328ecdfd7cbab561fcddbc688e
krishnareddygari25 <krishnareddygarianusha@gmail.com>
2023-11-09 21:58:02 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def anusha(request):
+    return render(request,'anusha.html')
+def anusha2(request):
+    return render(request,'anusha2.html')
+def anusha3(request):
+    return render(request,'anusha3.html')
+def anusha4(request):
+    return render(request,'anusha4.html')
---
+++ html4/urls.py
@@ -0,0 +1,27 @@
+"""
+URL configuration for html4 project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from app.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('anusha/',anusha,name='anusha'),
+    path('anusha2/',anusha2,name='anusha2'),
+    path('anusha3/',anusha3,name='anusha3'),
+    path('anusha4/',anusha4,name='anusha4'),
+]
